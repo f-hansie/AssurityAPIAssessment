9e14f74c81a2b2b208e62bd496d735b964f2307f
f-hansie <maphanga1988@gmail.com>
2022-09-12 15:35:05 +0200

    added assert on catch exception

--- src/test/java/StepDefination/getCategoryDetailsStepDef.java
+++ src/test/java/StepDefination/getCategoryDetailsStepDef.java
@@ -3,14 +3,16 @@ package StepDefination;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.plugin.event.Node;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.hamcrest.Matcher;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
 
-public class getCategoryDetailsTest {
+public class getCategoryDetailsStepDef {
 
     // declare validate response variable
     private ValidatableResponse validatableResponse;
@@ -41,36 +43,40 @@ public class getCategoryDetailsTest {
     }
 
     @When("the response must return name tag (.*)$")
-    public void the_response_must_return_name_tag_carbon_credits(String name) throws Exception {
+    public void the_response_must_return_name_tag_carbon_credits(String name){
 
         try {
             // validate Name value
             validatableResponse.assertThat().body("Name",equalTo(name));
         }catch (Exception e){
-            throw new Exception(e.getMessage());
+            //throw new Exception(e.getMessage());
+            System.out.println(e.getMessage());
+            Assert.fail();
         }
 
     }
 
     @When("the response must return boolean value (.*)$")
-    public void the_response_must_return_boolean_value_true(boolean canRelist) throws Exception {
+    public void the_response_must_return_boolean_value_true(boolean canRelist){
 
         try {
             // validate CanRelist value
             validatableResponse.assertThat().body("CanRelist",equalTo(canRelist));
         }catch (Exception e){
-            throw new Exception(e.getMessage());
+            System.out.println(e.getMessage());
+            Assert.fail();
         }
 
     }
     @Then("the response must return category description (.*)$")
-    public void the_response_must_return_category_description_good_position_in_category(String description) throws Exception{
+    public void the_response_must_return_category_description_good_position_in_category(String description){
 
         try {
             //validate description value
             validatableResponse.assertThat().body(containsStringIgnoringCase(description));
         }catch (Exception e){
-            throw new Exception(e.getMessage());
+            System.out.println(e.getMessage());
+            Assert.fail();
         }
     }
 }
